7fe45e0c0a6fa9ca82becded04e1138f587b2728
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-10-10 12:26:23 +0300

    pass directories to redux

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import userReducer from "./user/userReducer";
 import cartReducer from "./cart/cartReducer";
+import directoryReducer from "./directory/directoryReducer";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 //https://www.npmjs.com/package/redux-persist
 const persistConfig = {
@@ -12,6 +13,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory:directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);

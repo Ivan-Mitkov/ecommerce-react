4c1ce14639d5dd50b3489e5b4ada899eeb2b39d1
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-10-17 19:09:29 +0300

    apollo client init set up

--- src/index.js
+++ src/index.js
@@ -6,13 +6,41 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import store, { persistor } from "./redux/store";
+//GRAPHQL
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { createHttpLink } from "apollo-link-http";
+import { gql, useQuery } from "@apollo/client";
+
+const client = new ApolloClient({
+  link: createHttpLink({ uri: "https://crwn-clothing.com" }),
+  cache: new InMemoryCache(),
+});
+
+//check if working
+client.query({
+  query: gql`
+    {
+      collection(id: "cjwuuj5bz000i0719rrtw5gqk") {
+        title
+        items {
+          id
+          name
+          price
+          imageUrl
+        }
+      }
+    }
+  `,
+}).then(res=>console.log(res));
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Router>
-        <App />
-      </Router>
-    </PersistGate>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <App />
+        </Router>
+      </PersistGate>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById("root")
 );
